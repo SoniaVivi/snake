850ead72c42953bfb1a03e9eefdd58a49e367d7c
Siona Vivian <SionaSVivian@outlook.com>
2021-05-08 05:31:17 -0700

    create basic game

--- app/javascript/packs/board.js
+++ app/javascript/packs/board.js
@@ -23,6 +23,8 @@ const board = (() => {
 
   const _selectSquare = (x, y) =>
     document.querySelector(`[data-x='${x}'][data-y='${y}']`);
+  const random_integer = () => Math.floor(Math.random() * 17);
+
   const newGrid = () => {
     for (let y = 0; y < 17; y += 1) {
       let rowElem = _createElem("div", `row ${y}`, "grid");
@@ -30,17 +32,53 @@ const board = (() => {
         _createElem("div", `square`, rowElem, { x, y });
       }
     }
+    const startButton = _createElem("button", "start-button", "grid");
+    startButton.textContent = "Start!";
+    return startButton;
   };
 
   const addSnake = (segment) => {
-    _selectSquare(segment.x, segment.y).classList.add("snake");
+    const square = _selectSquare(segment.x, segment.y);
+    if (square.classList.contains("snake")) {
+      return "collision";
+    }
+    square.classList.add("snake");
+    if (square.textContent == "●") {
+      square.textContent = "";
+      square.classList.remove("food");
+      addFood();
+      return true;
+    }
+    return false;
   };
 
   const removeSnake = (segment) => {
     _selectSquare(segment.x, segment.y).classList.remove("snake");
   };
 
-  return { newGrid, addSnake, removeSnake };
+  const addFood = () => {
+    let x = random_integer();
+    let y = random_integer();
+    if (_selectSquare(x, y).classList.contains("snake")) {
+      return addFood();
+    } else {
+      const food = _selectSquare(x, y);
+      food.textContent = "●";
+      food.classList.add("food");
+    }
+  };
+
+  const clear = () => {
+    document
+      .querySelectorAll(".snake")
+      .forEach((elem) => elem.classList.remove("snake"));
+    document.querySelectorAll(".food").forEach((elem) => {
+      elem.classList.remove("food");
+      elem.textContent = "";
+    });
+  };
+
+  return { newGrid, addSnake, removeSnake, addFood, clear };
 })();
 
 export default board;
--- app/javascript/packs/game.js
+++ app/javascript/packs/game.js
@@ -2,9 +2,17 @@ import board from "./board";
 
 const game = (() => {
   let _initialRender = true;
+  let _firstGame = true;
   let _direction = "up";
   let _size = 1;
   let _snake = [{ x: 7, y: 7 }];
+  let _ongoing = false;
+  let _scoreElem;
+  let _startButton;
+  const _updateScore = () =>
+    (_scoreElem.textContent = `${
+      _scoreElem.textContent.split(" ")[0]
+    } ${_size}`);
   const _inBounds = (position) => position <= 16 && position >= 0;
   const _addSegment = () => {
     const moves = {
@@ -17,6 +25,7 @@ const game = (() => {
     let x = lastSegment.x + moves[_direction][0];
     let y = lastSegment.y + moves[_direction][1];
     if (!(_inBounds(x) && _inBounds(y))) {
+      _endGame();
       return false;
     }
     let newSegment = { x, y };
@@ -24,44 +33,85 @@ const game = (() => {
     return newSegment;
   };
   const _removeSegment = () => _snake.shift();
+  const _isInverseDirection = (input) => {
+    const inverses = {
+      up: { down: true },
+      down: { up: true },
+      left: { right: true },
+      right: { left: true },
+    };
+    return _direction in inverses[input] && _snake.length != 1 ? true : false;
+  };
   const _changeDirection = (key) => {
-    console.log(key);
     const keys = {
       ArrowUp: "up",
       ArrowDown: "down",
       ArrowLeft: "left",
       ArrowRight: "right",
     };
-    if (!key.repeat) {
+    if (!key.repeat && !_isInverseDirection(keys[key.key])) {
       _direction = keys[key.key];
     }
   };
-  const gameLoop = () => {
+  const _endGame = () => (_ongoing = !_ongoing);
+  const _gameLoop = () => {
+    if (!_ongoing) {
+      return;
+    }
     if (_snake.length == _size) {
       const newSegment = _addSegment();
       if (newSegment) {
-        board.addSnake(newSegment);
+        const result = board.addSnake(newSegment);
+        if (result == true) {
+          _size += 1;
+          _updateScore();
+        } else if (result == "collision") {
+          _endGame();
+        }
         board.removeSnake(_removeSegment());
       }
     } else if (_snake.length < _size) {
       board.addSnake(_addSegment());
     }
-    window.setTimeout(() => gameLoop(), 250);
+    window.setTimeout(() => _gameLoop(), 100);
+  };
+  const _restartGame = () => {
+    _size = 1;
+    _snake = [{ x: 7, y: 7 }];
+    _direction = "up";
+    _updateScore();
+    board.clear();
+    board.addFood();
+    board.addSnake(_addSegment());
+  };
+  const _getStartButton = (elem) => {
+    _startButton = elem;
+    document.addEventListener("mousedown", () => {
+      if (!_firstGame) {
+        _restartGame();
+      }
+      window.setTimeout(() => start(), 2000);
+    });
   };
 
   const render = () => {
     if (_initialRender) {
-      board.newGrid();
+      _getStartButton(board.newGrid());
       _initialRender = false;
     }
-    document.addEventListener("keydown", (e) => _changeDirection(e));
-    gameLoop();
+    board.addFood();
+    board.addSnake(_addSegment());
   };
 
-  // const _incrementTimer = () => {
-  // };
-
-  return { render };
+  const start = () => {
+    document.addEventListener("keydown", (e) => _changeDirection(e));
+    _ongoing = true;
+    _removeSegment();
+    _gameLoop();
+    _scoreElem = document.querySelector(".score-display");
+    _firstGame = false;
+  };
+  return { start, render };
 })();
 
 window.onload = () => {
